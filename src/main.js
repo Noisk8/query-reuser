b441d012a12590b7d5aaeeeb34ba4b0b84001909
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2026-07-22 10:12:02 +0200

    Automate message loading instead of hardcoding

--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
 import { createI18n } from "vue-banana-i18n";
-import en from "./i18n/en.json";
-import es from "./i18n/es.json";
-import qqq from "./i18n/qqq.json";
 import "@wikimedia/codex/dist/codex.style.css";
 import "./assets/main.css";
 import { getInitialLocale, applyLocale } from "./composables/useLanguage.js";
 
-const messages = { en, es, qqq };
+const localeFiles = import.meta.glob("./i18n/*.json", { eager: true });
+
+const messages = Object.fromEntries(
+  Object.entries(localeFiles)
+    .filter(([path]) => !path.endsWith("/qqq.json"))
+    .map(([path, module]) => [
+      path.match(/\/([^/]+)\.json$/)[1],
+      module.default,
+    ])
+);
+
 const initialLocale = getInitialLocale();
 applyLocale(initialLocale);
 
